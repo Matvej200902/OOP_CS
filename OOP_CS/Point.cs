6446938c2a6bd9ed30d6b476cc1bf1f668f6c70a
tabta <tabta@TABTABUR>
2025-12-02 20:18:00 +0900

    point

---
+++ OOP_CS/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CS {
+	internal class Point {
+		double x;
+		double y;
+
+		public double GetX()
+		{
+			return x;
+		}
+		public double GetY()
+		{
+			return y;
+		}
+
+		public void SetX(double x)
+		{
+			this.x = x;
+		}
+		public void SetY(double y)
+		{
+			this.y = y;
+		}
+		public void Print()
+		{
+			Console.WriteLine($"X = {x}\t Y = {y}");
+		}
+
+		public double X {
+			get
+			{
+				return x;
+			}
+
+			set
+			{
+				x = value;
+			}
+		}
+		public double Y {
+			get
+			{
+				return y;
+			}
+
+			set
+			{
+				y = value;
+			}
+		}
+
+
+	}
+}
--- OOP_CS/Program.cs
+++ OOP_CS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,15 @@ namespace OOP_CS {
 	internal class Program {
 		static void Main(string[] args)
 		{
+			Point A = new Point();
+			//A.SetX(2);
+			//A.SetY(3);
+			A.X = 22;
+			A.Y = 33;
+			A.Print();
+
+			
+
 		}
 	}
 }
