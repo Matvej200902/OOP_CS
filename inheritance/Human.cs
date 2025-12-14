05cdeda3768cfb01d9de4c2fa7bfebd23d943c5c
tabta <tabta@TABTABUR>
2025-12-14 15:18:26 +0900

    inheritance d1

---
+++ inheritance/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Human
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public int Age { get; set; }
+		public Human(string lastname, string firstname, int age)
+		{
+			LastName = lastname;
+			FirstName = firstname;
+			Age = age;
+			Console.WriteLine($"HConstructor: {GetHashCode()}");
+		}
+		~Human()
+		{
+			Console.WriteLine($"HDestructor: {GetHashCode()}");
+		}
+
+		public void Info()
+		{
+			Console.WriteLine($"{LastName} {FirstName}{Age}");
+		}
+	}
+}
---
+++ inheritance/academymember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+	internal class AcademyMember:Human
+	{
+		public string Speciality { get; set; }
+		public AcademyMember
+			(
+			string lastname, string firstname, int age,
+			string speciality
+			):base(lastname, firstname, age)
+
+		{
+			this.Speciality = speciality;
+		}
+	}
+}
